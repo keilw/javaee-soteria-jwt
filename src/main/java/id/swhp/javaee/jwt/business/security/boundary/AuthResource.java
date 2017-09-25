9b9f3fd7a1f0ea323d6eaa2723020069de869650
swhp <sukma.wardana@mail.com>
2017-09-25 16:03:04 +0700

    Resource to authenticate user and generate JWT token

---
+++ src/main/java/id/swhp/javaee/jwt/business/security/boundary/AuthResource.java
@@ -0,0 +1,41 @@
+package id.swhp.javaee.jwt.business.security.boundary;
+
+import java.util.Arrays;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Sukma Wardana
+ * @since 1.0
+ */
+@Stateless
+@Path("tokens")
+public class AuthResource {
+
+    @Inject
+    JWTStore jwtStore;
+
+    /**
+     *
+     * @param credential in json should be {"username": "...", "password": "..."}
+     * @return JWT token
+     */
+    @POST
+    public Response authenticate(JsonObject credential) {
+        // TODO: Should compare user credentials on the database.
+        String username = credential.getString("username");
+        String password = credential.getString("password");
+
+        // TODO: Groups should retrieve from database based on authenticate user.
+        String token = this.jwtStore.generateToken(username, Arrays.asList("ADMIN", "MEMBER"));
+
+        return Response.ok().header(AUTHORIZATION, "Bearer " + token).build();
+    }
+}
